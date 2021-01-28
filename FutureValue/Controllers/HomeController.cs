e4b6312d01eaee14d0dd2492844bb943653cab9e
Pacifique Ntahurunga <np@npacifique.us>
2021-01-27 22:43:52 -0500

    feat:upload Ch02Ex1

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            ViewBag.FV = 0.0;
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0.0;
+            }
+
+            return View(model);
+        }
+    }
+}
